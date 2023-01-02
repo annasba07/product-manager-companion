4bd3c889bc66f6830912df430ef5f4927b5b2652
annasba07 <ab722@cornell.edu>
2023-01-02 20:46:21 +0300

    mess

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 # app/routes.py
 
 from flask import Blueprint, render_template, request, flash, redirect, url_for
+import json
 from app import utils
 
 bp = Blueprint('routes', __name__)
@@ -10,18 +11,29 @@ def index():
     return render_template('index.html')
 
 
-@bp.route('/chat')
+
+
+@bp.route('/chat', methods=['GET', 'POST'])
 def chat():
     topic = request.args.get('topic')
-    messages = []
+    messages = request.args.get('messages', [])
+    if request.method == 'POST':
+        question = request.form['question']
+        messages = json.loads(request.form['messages'])
+        response = utils.get_response(question)
+        messages.append(question)
+        messages.append(response)
     return render_template('chat.html', topic=topic, messages=messages)
 
 
 
 
+
+
 @bp.route('/questions', methods=['POST'])
-def questions(messages):
+def questions():
     question = request.form['question']
+    messages = json.loads(request.form['messages'])
     response = utils.get_response(question)
     messages.append(question)
     messages.append(response)
