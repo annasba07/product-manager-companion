faeb85b05c8bb9bcb94b56e9e922de9f79e6dccb
annasba07 <ab722@cornell.edu>
2023-01-02 17:39:33 +0300

    update

--- app.py
+++ app.py
@@ -1,42 +1,74 @@
-# Import the `BeautifulSoup` and `requests` libraries
-from bs4 import BeautifulSoup
-import requests
+import os
 
-# Define the URL of the website that you want to scrape
-url = "https://program.reforge.com/programs/product-series-eg"
+import openai
+from flask import Flask, redirect, render_template, request, url_for
 
-# Use the `requests` library to send a GET request to the website
-response = requests.get(url)
-
-# Parse the HTML content of the response using the `BeautifulSoup` library
-soup = BeautifulSoup(response.text, "html.parser")
-
-# Find all of the `a` elements on the page that have an `href` attribute
-links = soup.find_all("a", href=True)
-
-# Iterate over the links
-for link in links:
-  # Get the `href` attribute of the link
-  page_url = link["href"]
-
-  # If the URL is a relative URL, convert it to an absolute URL
-  if page_url.startswith("/"):
-    page_url = url + page_url
-
-  # Send a GET request to the page URL
-  page_response = requests.get(page_url)
-
-  # Parse the HTML content of the page using the `BeautifulSoup` library
-  page_soup = BeautifulSoup(page_response)
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-    # Extract the text from the page using the `get_text()` method
-page_text = page_soup.get_text()
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        table1name = "Employee"
+        table1fields = "id, name, department_id"
+        
+        query = request.form["query"]
+        print(generate_single_sql_prompt(table1name,table1fields,table2name,table2fields, table3name, table3fields))
+        response = openai.Completion.create(
+            model="code-davinci-002",
+            prompt=generate_single_sql_prompt(table1name,table1fields,table2name,table2fields, table3name, table3fields),
+            temperature=0,
+            max_tokens=150,
+            stop=";",
+           
+
+        )
+        print(response)
+        
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result="Select "+ str(result))
 
 
-# Define the path and filename for the file
-# file_path = "data/page_{}.txt".format(page_number)
 
-# Open the file for writing using the `w` mode
-# with open(file_path, "w") as file:
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
+
+
+
+def generate_single_sql_prompt(table1name, table1fieldnames,table2name,table2fieldnames, table3name, table3fieldnames):
+    return """Provide SQL tables, with their properties:
+    {}({})
+    {}({})
+    {}({})
+    A query to list the names of the departments which employed more than 10 employees in the last 3 months
+SELECT 
+    """.format(table1name,table1fieldnames,table2name,table2fieldnames,table3name,table3fieldnames)
+
+
+def takeindata():
+    print('helloworld')
+
+
+#schema_str = "Employees (id, name, department, salary); Departments (id, name, location)"
+#schema_list = schema_str.split(";")
+#tables = []
+#for item in schema_list:
+#  table_name, fields = item.split("(")
+#  fields = fields.replace(")", "").split(",")
+#  tables.append({
+#    "name": table_name,
+#    "fields": fields,
+#  })
 
