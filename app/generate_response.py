0152256394aa9154eecd485ee37a8bfa298f5d3f
annasba07 <ab722@cornell.edu>
2023-01-08 02:48:52 +0300

    update

--- app/generate_response.py
+++ app/generate_response.py
@@ -6,3 +6,38 @@ openai.api_key = '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'
 
 from openai.embeddings_utils import get_embedding, cosine_similarity
 
+
+question = "what is the future of product management? \n"
+query = "Q: " + question + " A: "
+
+embeddingpm = get_embedding(
+        question,
+        engine="text-embedding-ada-002"
+    )
+
+df["similarities"] = df.embeddings.apply(lambda x: np.dot(np.array(embeddingpm), np.array(x)))
+df = df.sort_values(by=['similarities'], ascending=False)
+df = df.reset_index(drop=True)
+
+
+context = df.combined[0]
+#+ df.combined[1] 
+#+ df.combined[2]  + df.combined[3]
+len(context)
+
+
+COMPLETIONS_MODEL = "text-davinci-002"
+
+prompt = """You are a product management expert. Answer the question in detail using the provided context, and if the answer is not contained in the text above then answer it how you normally would. Explain things in a lot of detail.   \n""" + context + query
+
+openai.Completion.create(
+    prompt=prompt,
+    temperature=0,
+    max_tokens=500,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    model=COMPLETIONS_MODEL
+)["choices"][0]["text"].strip(" \n")
+
+
