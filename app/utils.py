98fb15ab7cdac49601a240a2bb4b79af3c300415
annasba07 <ab722@cornell.edu>
2023-01-08 19:47:14 +0300

    update

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,25 @@
 # app/utils.py
 from app import generate_response
+from flask import session
 
 
 def get_response(question):
     # Implement logic for generating a response to the user's question here
     response = question
     response = response + response
-    return response
+    context = 'provide a lot of detail'
+
+    #print question in terminal
+    print(session['messages'][-5:])
+
+    #take the last 5 messages from the session
+    #similarInfo = session['messages'][-5:]
+
+    get_answer = generate_response.get_answer(question, context)
+
+    
+
+    return get_answer
 
 
 
