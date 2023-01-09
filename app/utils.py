ba1f46144c2f9af2508ea2045638c6cf89076bd0
annasba07 <ab722@cornell.edu>
2023-01-09 03:37:11 +0300

    update

--- app/utils.py
+++ app/utils.py
@@ -13,15 +13,18 @@ def get_response(question):
     print(session['messages'][-6:])
 
     #take the last 5 messages from the session
-    similarInfo = session['messages'][-6:]
+    similarInfo = session['messages']
+
+    for i in similarInfo:
+        context.append("Q: " + i + " A: ")
 
     #take the last 6 message and join them together by saying one message is "Q: " and the other is "A: "
     context = "Q: " + similarInfo[0] + " A: " + similarInfo[1] + " Q: " + similarInfo[2] + " A: " + similarInfo[3] + " Q: " + similarInfo[4] + " A: " + similarInfo[5]
     print(context)
 
-    #take the last 6 messages and join them together by saying one message is "Q: " and the other is "A: " if there are less than 6 messages, then just use the messages that are there
-    if len(similarInfo) < 6:
-        context = "Q: " + similarInfo[0] + " A: " + similarInfo[1] + " Q: " + similarInfo[2] + " A: " + similarInfo[3] + " Q: " + similarInfo[4] + " A: " + similarInfo[5]
+    #for every item in similarinfo add it to the context
+    
+   
 
     get_answer = generate_response.get_answer(question, context)
 
